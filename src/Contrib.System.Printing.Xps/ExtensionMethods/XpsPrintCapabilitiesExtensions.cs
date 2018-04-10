35dd3d5ccfcfba796091b46b943cfb7699b9b27e
Andreas Niedermair <andreas@niedermair.name>
2018-04-10 12:32:01 +0200

    returning input bin IXpsFeature instance instead of all possible

--- src/Contrib.System.Printing.Xps/ExtensionMethods/XpsPrintCapabilitiesExtensions.cs
+++ src/Contrib.System.Printing.Xps/ExtensionMethods/XpsPrintCapabilitiesExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
 
 namespace Contrib.System.Printing.Xps.ExtensionMethods
@@ -8,30 +6,20 @@ namespace Contrib.System.Printing.Xps.ExtensionMethods
   public static class XpsPrintCapabilitiesExtensions
   {
     /// <exception cref="ArgumentNullException"><paramref name="xpsPrintCapabilities"/> is <see langword="null"/></exception>
-    [NotNull]
-    [ItemNotNull]
-    public static IXpsFeature[] GetInputBinXpsFeatures([NotNull] this IXpsPrintCapabilities xpsPrintCapabilities)
+    [Pure]
+    [CanBeNull]
+    public static IXpsFeature GetInputBinXpsFeature([NotNull] this IXpsPrintCapabilities xpsPrintCapabilities)
     {
       if (xpsPrintCapabilities == null)
       {
         throw new ArgumentNullException(nameof(xpsPrintCapabilities));
       }
 
-      var inputBinXpsFeatures = xpsPrintCapabilities.GetInputBinXpsFeaturesImpl();
-      var result = inputBinXpsFeatures.ToArray();
-
-      return result;
-    }
-
-    [NotNull]
-    [ItemNotNull]
-    internal static IEnumerable<IXpsFeature> GetInputBinXpsFeaturesImpl([NotNull] this IXpsPrintCapabilities xpsPrintCapabilities)
-    {
       {
         var pageInputBinXpsFeature = xpsPrintCapabilities.GetXpsFeature(PrintCapabilitiesReader.PageInputBinXName);
         if (pageInputBinXpsFeature != null)
         {
-          yield return pageInputBinXpsFeature;
+          return pageInputBinXpsFeature;
         }
       }
 
@@ -39,7 +27,7 @@ namespace Contrib.System.Printing.Xps.ExtensionMethods
         var documentInputBinXpsFeature = xpsPrintCapabilities.GetXpsFeature(PrintCapabilitiesReader.DocumentInputBinXName);
         if (documentInputBinXpsFeature != null)
         {
-          yield return documentInputBinXpsFeature;
+          return documentInputBinXpsFeature;
         }
       }
 
@@ -47,9 +35,11 @@ namespace Contrib.System.Printing.Xps.ExtensionMethods
         var jobInputBinXpsFeature = xpsPrintCapabilities.GetXpsFeature(PrintCapabilitiesReader.JobInputBinXName);
         if (jobInputBinXpsFeature != null)
         {
-          yield return jobInputBinXpsFeature;
+          return jobInputBinXpsFeature;
         }
       }
+
+      return null;
     }
   }
 }
