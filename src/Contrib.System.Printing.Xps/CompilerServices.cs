36216655209e1f590a1f15a1440c08377da7f5a9
Andreas Niedermair <andreas@niedermair.name>
2018-04-13 16:04:23 +0200

    added CompilerServices
    moooooaaaaahhhh features in .NET 4.0 :)

---
+++ src/Contrib.System.Printing.Xps/CompilerServices.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Runtime.CompilerServices
+{
+  [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+  internal class CallerMemberNameAttribute : Attribute { }
+
+  [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+  internal class CallerFilePathAttribute : Attribute { }
+
+  [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+  internal class CallerLineNumberAttribute : Attribute { }
+}
