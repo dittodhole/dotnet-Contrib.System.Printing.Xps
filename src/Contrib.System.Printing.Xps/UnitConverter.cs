5b93063d8512bc5656be397c27b0688d2536ff5d
Andreas Niedermair <andreas@niedermair.name>
2018-04-13 10:36:24 +0200

    added UnitConverter

---
+++ src/Contrib.System.Printing.Xps/UnitConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Contrib.System.Printing.Xps
+{
+  /// <remarks>Copied from MS.Internal.Printing.Configuration.UnitConverter, ReachFramework</remarks>
+  public class UnitConverter
+  {
+    private UnitConverter()
+    {
+    }
+
+    public static double LengthValueFromMicronToDIP(int micronValue)
+    {
+      if (micronValue == int.MinValue)
+        return double.MinValue;
+      return (double) micronValue / 25400.0 * 96.0;
+    }
+
+    public static int LengthValueFromDIPToMicron(double dipValue)
+    {
+      return (int) (dipValue / 96.0 * 25400.0 + 0.5);
+    }
+  }
+}
