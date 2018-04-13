f337f27884106b283d39fe0d86b8143fe1553347
Andreas Niedermair <andreas@niedermair.name>
2018-04-13 16:04:45 +0200

    made UnitConverter a transparent proxy to original implementation

--- src/Contrib.System.Printing.Xps/UnitConverter.cs
+++ src/Contrib.System.Printing.Xps/UnitConverter.cs
@@ -1,22 +1,56 @@
-﻿namespace Contrib.System.Printing.Xps
+﻿using System;
+using System.Linq;
+using System.Printing;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace Contrib.System.Printing.Xps
 {
-  /// <remarks>Copied from MS.Internal.Printing.Configuration.UnitConverter, ReachFramework</remarks>
-  public class UnitConverter
+  public static class UnitConverter
   {
-    private UnitConverter()
+    [NotNull]
+    private static Type FindInternalType()
     {
+      var printTicket = new PrintTicket(); // to force loading of ReachFramework
+      var unitConverterType = AppDomain.CurrentDomain.GetAssemblies()
+                                       .Select(assembly => assembly.GetType("MS.Internal.Printing.Configuration.UnitConverter"))
+                                       .FirstOrDefault(type => type != null);
+
+      return unitConverterType;
+    }
+
+    [NotNull]
+    private static MethodInfo FindInternalMethod([CallerMemberName] string callerMemberName = "")
+    {
+      var unitConverterType = UnitConverter.FindInternalType();
+      var methodInfo = unitConverterType.GetMethod(callerMemberName);
+
+      return methodInfo;
     }
 
     public static double LengthValueFromMicronToDIP(int micronValue)
     {
-      if (micronValue == int.MinValue)
-        return double.MinValue;
-      return (double) micronValue / 25400.0 * 96.0;
+      var methodInfo = UnitConverter.FindInternalMethod();
+      var result = methodInfo.Invoke(null,
+                                     new object[]
+                                     {
+                                       micronValue
+                                     });
+
+      return (double) result;
     }
 
     public static int LengthValueFromDIPToMicron(double dipValue)
     {
-      return (int) (dipValue / 96.0 * 25400.0 + 0.5);
+      var methodInfo = UnitConverter.FindInternalMethod();
+      var result = methodInfo.Invoke(null,
+                                     new object[]
+                                     {
+                                       dipValue
+                                     });
+
+      return (int) result;
     }
   }
 }
