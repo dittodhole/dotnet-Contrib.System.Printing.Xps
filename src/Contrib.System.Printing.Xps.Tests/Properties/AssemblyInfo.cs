1155c1de34de85b4717fe0514abc48cbb88193e7
Andreas Niedermair <andreas@niedermair.name>
2018-04-13 16:08:03 +0200

    added test project

---
+++ src/Contrib.System.Printing.Xps.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Contrib.System.Printing.Xps.Tests")]
+[assembly: AssemblyDescription("Test project for Contrib.System.Printing.Xps")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andreas Niedermair (@dittodhole)")]
+[assembly: AssemblyProduct("Contrib.System.Printing.Xps.Tests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("787c5ab4-3906-4202-95f0-e8e8f085d557")]
---
+++ src/Contrib.System.Printing.Xps.Tests/UnitConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Contrib.System.Printing.Xps.Tests
+{
+  [TestFixture]
+  public class UnitConverterTests
+  {
+    [Test]
+    public void LengthValueFromMicronToDIP_Should_Succeed()
+    {
+      var value = UnitConverter.LengthValueFromMicronToDIP(100);
+
+      Assert.IsTrue(value > double.Epsilon);
+    }
+
+    [Test]
+    public void LengthValueFromDIPToMicron_Should_Succeed()
+    {
+      var value = UnitConverter.LengthValueFromDIPToMicron(100d);
+
+      Assert.IsTrue(value > 0);
+    }
+  }
+}
