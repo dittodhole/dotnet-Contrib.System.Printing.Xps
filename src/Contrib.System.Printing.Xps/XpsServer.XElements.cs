f8a3256843598df20730a10073e3c476113b01d9
Andreas Niedermair <andreas@niedermair.name>
2018-04-20 10:54:44 +0200

    added XpsServer.XElements

---
+++ src/Contrib.System.Printing.Xps/XpsServer.XElements.cs
@@ -0,0 +1,25 @@
+﻿/** @pp
+ * rootnamespace: Contrib.System.Printing.Xps
+ */
+namespace Contrib.System.Printing.Xps
+{
+  using global::System.Xml.Linq;
+  using global::JetBrains.Annotations;
+
+  public partial class XpsServer
+  {
+    /// <summary>
+    ///   <psf:PrintCapabilities/>
+    /// </summary>
+    [PublicAPI]
+    [NotNull]
+    public static XElement PrintCapabilitiesElement => new XElement(XpsServer.PrintCapabilitiesName);
+
+    /// <summary>
+    ///   <psf:PrintTicket/>
+    /// </summary>
+    [PublicAPI]
+    [NotNull]
+    public static XElement PrintTicketElement => new XElement(XpsServer.PrintTicketName);
+  }
+}
