4d583b95e057ad924b05c3f108e327dac0840271
Andreas Niedermair <dev@andreas.niedermair.name>
2018-05-29 10:44:46 +0200

    refined tests :trollface:

--- src/Contrib.System.Printing.Xps.Tests/ExtensionMethods/XElementExtensionsTests.cs
+++ src/Contrib.System.Printing.Xps.Tests/ExtensionMethods/XElementExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
 using Contrib.System.Printing.Xps.ExtensionMethods;
 using NUnit.Framework;
 
@@ -7,49 +9,69 @@ namespace Contrib.System.Printing.Xps.Tests.ExtensionMethods
   [TestFixture]
   public class XElementExtensionsTests
   {
-    [TestCase("name", "name")]
-    [TestCase("{http://my.scheme.com}name", "ns0000:name")]
-    public void ReduceName_Should_Succeed(string expandedName,
-                                          string expectedShortName)
+    [TestCaseSource(nameof(XElementExtensionsTests.ReduceName_Should_Succeed_TestCases))]
+    public string ReduceName_Should_Succeed(XName name)
     {
       var element = new XElement("element");
-      var name = XName.Get(expandedName);
-      var actualReducedName = element.ReduceName(name);
+      var reducedName = element.ReduceName(name);
 
-      Assert.AreEqual(expectedShortName,
-                      actualReducedName);
+      return reducedName;
     }
 
-    [TestCase("name", null)]
-    [TestCase("{http://my.scheme.com}name", "ns0000")]
-    public void EnsurePrefixRegistrationOfNamespace_Should_Succeed(string expandedName,
-                                                                   string expectedPrefix)
+    private static IEnumerable<TestCaseData> ReduceName_Should_Succeed_TestCases
+    {
+      get
+      {
+        yield return new TestCaseData(XName.Get("name")).Returns("name");
+        yield return new TestCaseData(XName.Get("{http://my.scheme.com}name")).Returns("ns0000:name");
+      }
+    }
+
+    [TestCaseSource(nameof(XElementExtensionsTests.EnsurePrefixRegistrationOfNamespace_Should_Succeed_TestCases))]
+    public string EnsurePrefixRegistrationOfNamespace_Should_Succeed(XName name)
     {
       var element = new XElement("element");
-      var name = XName.Get(expandedName);
-      var actualPrefix = element.EnsurePrefixRegistrationOfNamespace(name);
+      var prefix = element.EnsurePrefixRegistrationOfNamespace(name);
 
-      Assert.AreEqual(expectedPrefix,
-                      actualPrefix);
+      return prefix;
     }
 
-    [TestCase("child1")]
-    [TestCase("16bpcSupport")]
-    public void FindElementByNameAttribute_Should_Succeed(string name)
+    private static IEnumerable<TestCaseData> EnsurePrefixRegistrationOfNamespace_Should_Succeed_TestCases
     {
-      var root = new XElement("root");
-      var child1 = new XElement("child1");
-      child1.SetAttributeValue(XpsServer.NameName,
-                               "child1");
-      root.Add(child1);
-      var child2 = new XElement("child2");
-      child2.SetAttributeValue(XpsServer.NameName,
-                               "16bpcSupport");
-      root.Add(child2);
+      get
+      {
+        yield return new TestCaseData(XName.Get("name")).Returns(null);
+        yield return new TestCaseData(XName.Get("{http://my.scheme.com}name")).Returns("ns0000");
+      }
+    }
 
+    [TestCaseSource(nameof(XElementExtensionsTests.FindElementByNameAttribute_Should_Succeed_TestCases))]
+    public void FindElementByNameAttribute_Should_Succeed(Func<XElement, string> getNameFn)
+    {
+      const string content = @"
+<root xmlns:ns0000=""http://my.scheme.com"">
+  <child1 name=""child1""/>
+  <child2 name=""16bpcSupport""/>
+  <child3 name=""ns0000:foo""/>
+</root>";
+
+      var document = XDocument.Parse(content);
+      var root = document.Root;
+
+      var name = getNameFn.Invoke(root);
       var result = root.FindElementByNameAttribute(name);
 
       Assert.IsNotNull(result);
+    }
+
+    private static IEnumerable<TestCaseData> FindElementByNameAttribute_Should_Succeed_TestCases
+    {
+      get
+      {
+        yield return new TestCaseData(new Func<XElement, string>(element => "child1"));
+        yield return new TestCaseData(new Func<XElement, string>(element => "16bpcSupport"));
+        yield return new TestCaseData(new Func<XElement, string>(element => element.ReduceName("{http://my.scheme.com}foo")));
+      }
     }
   }
 }
