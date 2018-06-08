5ae30fdb34617ee5c1bdc81d8a948274fad3ca3c
Andreas Niedermair <dev@andreas.niedermair.name>
2018-06-08 14:06:55 +0200

    added tests for XpsName.Get(string)

---
+++ src/Contrib.System.Printing.Xps.Tests/XpsNameTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Contrib.System.Printing.Xps.Tests
+{
+  using global::NUnit.Framework;
+
+  [TestFixture]
+  public class XpsNameTests
+  {
+    [TestCaseSource(nameof(XpsNameTests.Get_TestCases))]
+    public void Get(string expandedName,
+                    XNamespace @namespace,
+                    string localName)
+    {
+      var xpsName = XpsName.Get(expandedName);
+
+      Assert.AreEqual(@namespace,
+                      xpsName.Namespace);
+      Assert.AreEqual(localName,
+                      xpsName.LocalName);
+    }
+
+    private static IEnumerable<TestCaseData> Get_TestCases
+    {
+      get
+      {
+        yield return new TestCaseData("{http://schemas.microsoft.com/windows/2003/08/printing/printschemaframework}PrintCapabilities",
+                                      XpsServer.PrinterSchemaFrameworkNamespace,
+                                      "PrintCapabilities");
+        yield return new TestCaseData("{http://schemas.microsoft.com/windows/2003/08/printing/printschemaframework}",
+                                      XpsServer.PrinterSchemaFrameworkNamespace,
+                                      string.Empty);
+        yield return new TestCaseData("PrintCapabilities",
+                                      XNamespace.None,
+                                      "PrintCapabilities");
+      }
+    }
+  }
+}
