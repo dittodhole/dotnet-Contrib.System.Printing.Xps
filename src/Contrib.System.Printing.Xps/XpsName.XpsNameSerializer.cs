61eaeaa600ab7c39f33c3828a02e2968f8ff3678
Andreas Niedermair <dev@andreas.niedermair.name>
2018-06-14 11:03:02 +0200

    placed XpsNameSerializer under XpsName

---
+++ src/Contrib.System.Printing.Xps/XpsName.XpsNameSerializer.cs
@@ -0,0 +1,69 @@
+﻿/** @pp
+ * rootnamespace: Contrib.System
+ */
+namespace Contrib.System.Printing.Xps
+{
+  using global::System;
+  using global::System.Runtime.Serialization;
+  using global::JetBrains.Annotations;
+
+#if CONTRIB_SYSTEM_PRINTING_XPS
+  public sealed
+#else
+  internal
+#endif
+  partial class XpsName
+  {
+    [Serializable]
+#if CONTRIB_SYSTEM_PRINTING_XPS
+    internal sealed
+#else
+    internal
+#endif
+    partial class XpsNameSerializer : IObjectReference,
+                                      ISerializable
+    {
+      /// <summary>
+      ///   Initializes a new instance of the <see cref="T:Contrib.System.Printing.Xps.XpsNameSerializer"/> class.
+      /// </summary>
+      /// <param name="serializationInfo"/>
+      /// <param name="streamingContext"/>
+      /// <exception cref="T:System.ArgumentNullException"><paramref name="serializationInfo"/> is <see langword="null"/>.</exception>
+      public XpsNameSerializer([NotNull] SerializationInfo serializationInfo,
+                               StreamingContext streamingContext)
+      {
+        if (serializationInfo == null)
+        {
+          throw new ArgumentNullException(nameof(serializationInfo));
+        }
+        this.ExpandedName = serializationInfo.GetString(nameof(this.ExpandedName));
+      }
+
+      [CanBeNull]
+      public string ExpandedName { get; }
+
+      /// <inheritdoc/>
+      object IObjectReference.GetRealObject(StreamingContext context)
+      {
+        XpsName result;
+        if (this.ExpandedName == null)
+        {
+          result = null;
+        }
+        else
+        {
+          result = XpsName.Get(this.ExpandedName);
+        }
+
+        return result;
+      }
+
+      /// <inheritdoc/>
+      void ISerializable.GetObjectData(SerializationInfo info,
+                                       StreamingContext context)
+      {
+        throw new NotSupportedException();
+      }
+    }
+  }
+}
